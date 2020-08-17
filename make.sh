64a827c7020ed1a56c1b2639301e8425227aad41
evuraan @ x1 <evuraan@gmail.com>
2020-08-16 20:01:55 -0700

    binary name for armv6l changed

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 mkdir -pv bin
 GOARCH=arm GOARM=7 go build -o ./bin/ipv6thingy-armv7l
-GOARCH=arm GOARM=6 go build -o ./bin/ipv6thingy-armipv6thingyl
+GOARCH=arm GOARM=6 go build -o ./bin/ipv6thingy-armv6l
 GOARCH=amd64 go build -o ./bin/ipv6thingy-x86_64
 GOARCH=amd64 go build -o ./bin/ipv6thingy-x86_64
 GOARCH=386 go build -o ./bin/ipv6thingy-386
